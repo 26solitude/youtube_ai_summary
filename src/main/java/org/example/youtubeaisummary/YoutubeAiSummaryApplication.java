2ce62a2d15452b7ce1880d144e2730f5c65549a2
hojang <26dev@naver.com>
2025-05-30 16:15:50 +0900

    Initial commit

---
+++ src/main/java/org/example/youtubeaisummary/YoutubeAiSummaryApplication.java
@@ -0,0 +1,13 @@
+package org.example.youtubeaisummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeAiSummaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeAiSummaryApplication.class, args);
+    }
+
+}
